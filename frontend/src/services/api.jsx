77790c4e8f0fb57383f4b860b078d807e76ea9e4
SHREYASH-2211 <137050866+SHREYASH-2211@users.noreply.github.com>
2025-07-20 01:41:10 +0530

    add

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+//   baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://educonnect-imfb.onrender.com/api',
   withCredentials: true,
 });
 // const API = axios.create({
